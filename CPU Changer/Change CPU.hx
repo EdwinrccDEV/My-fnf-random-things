4ed25b28666bb095a22b1cad59b00add054372c7
Edwin_rcc [DEV] <edwincastchi14@gmail.com>
2025-08-15 00:18:14 -0700

    Delete CPU Changer/Change CPU.hx

--- CPU Changer/Change CPU.hx
+++
@@ -1,8 +0,0 @@
-function onEvent(event) {
-  if(event.event.name == "Change CPU")
-  {
-  var ch = event.event.params[0];
-  var isCPU = event.event.params[1];
- strumLines.members[ch].cpu = isCPU;
-  }
-}
